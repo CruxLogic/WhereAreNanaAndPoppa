9a1be2f9545e5a2dea750d1c9b55d14ef7b3d2ea
PeteBanks <PeteBanks@users.noreply.github.com>
2026-05-09 22:10:33 +1200

    Move map attribution to top-left so it isn't hidden by header (#4)

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -66,7 +66,7 @@ export function Globe({
       touchPitch: false,
     });
     mapRef.current = map;
-    map.addControl(new maplibregl.AttributionControl({ compact: true }), "top-right");
+    map.addControl(new maplibregl.AttributionControl({ compact: true }), "top-left");
 
     map.touchZoomRotate.disableRotation();
 
